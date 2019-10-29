0a92e544e7695aa6b5c4219aa4a34ba978a1fa3f
Diego Maroldi Barreiros <diegomaroldi@gmail.com>
2019-10-29 10:04:51 -0300

    No arquivo MainActivity adicionar a implementação da interface View.OnClickListener e adicionar o método onClick.

--- app/src/main/java/br/edu/ifsp/scl/dicesdm/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/dicesdm/MainActivity.java
@@ -3,12 +3,13 @@ package br.edu.ifsp.scl.dicesdm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Random geradorRandomico;
 
@@ -21,5 +22,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         geradorRandomico = new Random(System.currentTimeMillis());
+
+        resultadoTextView = findViewById(R.id.resultadoTextView);
+
+        jogarDadoButton = findViewById(R.id.jogarDadoButton);
+        jogarDadoButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.jogarDadoButton) {
+            int resultado = geradorRandomico.nextInt(6) + 1;
+            resultadoTextView.setText("Face sorteada: " + resultado);
+        }
     }
 }
