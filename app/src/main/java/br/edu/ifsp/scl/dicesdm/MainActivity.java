7437d6647eea0d56f4558a76fa32e42c8b62cd08
Diego Maroldi Barreiros <diegomaroldi@gmail.com>
2019-10-29 09:56:33 -0300

    No arquivo MainActivity adicionar os seguintes atributos: geradorRandomico, resultadoTextView e jogarDadoButton.

--- app/src/main/java/br/edu/ifsp/scl/dicesdm/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/dicesdm/MainActivity.java
@@ -3,12 +3,23 @@ package br.edu.ifsp.scl.dicesdm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Random geradorRandomico;
+
+    private TextView resultadoTextView;
+    private Button jogarDadoButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        geradorRandomico = new Random(System.currentTimeMillis());
     }
 }
